f3a78fbadc2f64b209c313af84468b52d11f496b
Niptlox <58343474+Niptlox@users.noreply.github.com>
2023-02-07 18:18:34 +0500

    ++

---
+++ data.py
@@ -0,0 +1,72 @@
+from typing import Dict
+
+CAPABILITY_PUNCH = 2
+CAPABILITY_SHOOT = 4
+CAPABILITY_PROTECT = 8
+CAPABILITY_MAGIC = 16
+
+ITEM_TOOL = 1
+ITEM_POISON = 2
+ITEM_EAT = 4
+
+EFFECT_INSTANT_HEALTH = 1
+
+S_HEALTH = "health"
+S_MANA = "mana"
+S_TYPE = "type"
+S_TITLE = "title"
+S_IMGID = "img_id"
+S_ANCESTOR = "ancestor"
+S_EFFECT = "effect"
+S_RESTORING_HEALTH = "restoring_health"
+S_CAPABILITIES = "capabilities"
+S_MAX_CELL_CNT = "max_cell_cnt"
+
+S_PUNCH_DAMAGE = "punch_damage"
+S_PUNCH_DAMAGE_MIN = "punch_damage_min"
+S_PUNCH_DAMAGE_MAX = "punch_damage_max"
+S_PUNCH_CHANCE = "punch_chance"
+
+Capabilities = {
+    "DefaultPunch": {S_TYPE: CAPABILITY_PUNCH, S_TITLE: "Default punch", S_PUNCH_CHANCE: 0.7, S_PUNCH_DAMAGE_MIN: 10,
+                     S_PUNCH_DAMAGE_MAX: 15},
+    "LightPunch": {S_ANCESTOR: "DefaultPunch", S_TITLE: "Light Punch", S_PUNCH_DAMAGE_MIN: 5, S_PUNCH_DAMAGE_MAX: 7}
+}
+Creatures = {
+    "BlackCat": {S_TITLE: "ЪУЪ", S_IMGID: 5, S_HEALTH: 50, S_CAPABILITIES: ["DefaultPunch", ]},
+    "CryCat": {S_TITLE: "Cry cat", S_IMGID: 6, S_HEALTH: 30, S_CAPABILITIES: ["LightPunch", ]},
+}
+
+# Виды ввода эффекта: "id effect"; {S_ANCESTOR: "id effect", }; ("id effect", steps); ("id effect", steps, mul_cof))
+Effects = {
+    "RestoreHealth": {S_TYPE: EFFECT_INSTANT_HEALTH, S_TITLE: "Восстановление жизни", S_HEALTH: 5}
+}
+
+Items = {
+    "Poison": {S_TYPE: ITEM_POISON, S_TITLE: "Poison", S_MAX_CELL_CNT: 8},
+    "HealthPoison": {S_ANCESTOR: "Poison", S_TITLE: "Health Poison", S_HEALTH: 30},
+    "HealthSmallPoison": {S_ANCESTOR: "HealthPoison", S_TITLE: "Health Small Poison", S_HEALTH: 10},
+    "HealthBigPoison": {S_ANCESTOR: "HealthPoison", S_TITLE: "Health Big Poison", S_HEALTH: 50},
+    "RestoreHealthPoison": {S_ANCESTOR: "Poison", S_TITLE: "Restore Health Poison", S_HEALTH: 10, S_EFFECT: ("RestoreHealth", 5)},
+}
+
+
+def unpack_option(option, options: Dict[str, dict]):
+    if option.get(S_ANCESTOR) is None:
+        return option
+    anc_option = options.get(option.get(S_ANCESTOR)).copy()
+    anc_option.update(option)
+    return anc_option
+
+
+def __unpack_options(options):
+    options = options.copy()
+    for key in options:
+        options[key] = unpack_option(options[key], options)
+    return options
+
+
+Capabilities = __unpack_options(Capabilities)
+Creatures = __unpack_options(Creatures)
+Effects = __unpack_options(Effects)
+Items = __unpack_options(Items)
--- main.py
+++ main.py
@@ -1,27 +1,13 @@
+from typing import List
+
 import pygame as pg
 import pygame.display
 import random
+from data import *
 
 pg.init()
 WSIZE = (960, 720)
 screen = pygame.display.set_mode(WSIZE, flags=pg.SRCALPHA)
-
-CAPABILITY_PUNCH = 2
-CAPABILITY_SHOOT = 4
-CAPABILITY_PROTECT = 8
-CAPABILITY_MAGIC = 16
-
-S_LIVES = "lives"
-S_MANA = "mana"
-S_CTYPE = "ctype"
-S_TITLE = "title"
-S_IMGID = "img_id"
-S_ANCESTOR = "ancestor"
-
-S_PUNCH_DAMAGE = "punch_damage"
-S_PUNCH_DAMAGE_MIN = "punch_damage_min"
-S_PUNCH_DAMAGE_MAX = "punch_damage_max"
-S_PUNCH_CHANCE = "punch_chance"
 
 PLAYER_IMG_ID = 0
 img_size = (220, 220)
@@ -30,24 +16,14 @@ shadow_img = pg.Surface(img_size).convert()
 shadow_img.fill("#64748baa")
 shadow_img.set_alpha(220)
 
-capabilities_options = {
-    "DefaultPunch": {S_CTYPE: CAPABILITY_PUNCH, S_TITLE: "Default punch", S_PUNCH_CHANCE: 0.7, S_PUNCH_DAMAGE_MIN: 10,
-                     S_PUNCH_DAMAGE_MAX: 15},
-    "LightPunch": {S_ANCESTOR: "DefaultPunch", S_PUNCH_DAMAGE_MIN: 5, S_PUNCH_DAMAGE_MAX: 7, S_TITLE: "Light Punch"}
-}
-creature_options = {
-    "BlackCat": {S_TITLE: "ЪУЪ", S_IMGID: 5, S_LIVES: 50, "capabilities": ["DefaultPunch", ]},
-    "CryCat": {S_TITLE: "Cry cat", S_IMGID: 6, S_LIVES: 30, "capabilities": ["LightPunch", ]},
-}
-
 
 def get_creature(creature_id):
-    options = creature_options[creature_id]
+    options = Creatures[creature_id]
     return Creature(options, get_capabilities(options["capabilities"]))
 
 
 def get_capability(cap_id):
-    return Capability(capabilities_options.get(cap_id))
+    return Capability(Capabilities.get(cap_id))
 
 
 def get_capabilities(cap_ids):
@@ -59,8 +35,8 @@ class Creature:
         self.options = options
         self.name = options[S_TITLE]
         self.img_id = options[S_IMGID]
-        self.max_lives = options[S_LIVES]
-        self.lives = self.max_lives
+        self.max_health = options[S_HEALTH]
+        self.health = self.max_health
         self.mana = options.get(S_MANA, -1)
         self.capabilities = capabilities
         self.alive = alive
@@ -69,34 +45,62 @@ class Creature:
         self.get_damage(damage)
 
     def get_damage(self, damage):
-        self.lives -= damage
-        if self.lives <= 0:
+        self.health -= damage
+        if self.health <= 0:
+            self.kill()
+
+    def add_health(self, health):
+        self.health = min(self.max_health, self.health + health)
+        if self.health <= 0:
             self.kill()
 
     def kill(self):
         self.alive = False
 
     @property
-    def lives_percent(self):
-        return self.lives / self.max_lives
+    def health_percent(self):
+        return self.health / self.max_health
 
     def draw(self, surface, pos):
         surface.blit(imgs[self.img_id], pos)
 
 
-class Button:
-    button_font = pg.font.SysFont("Arial", 20, )
+class Label:
+    _font = pg.font.SysFont("Arial", 20, )
 
-    def __init__(self, rect, text, on_click=None, text_color="BLACK", background="WHITE", font=button_font):
+    def __init__(self, rect, text, text_color="BLACK", background="WHITE", font=_font):
         self.rect = pg.Rect(rect)
+        self.font = font
+        self.background = background
+        self.text_color = text_color
         self.surface = pg.Surface(self.rect.size)
-        self.surface.fill(background)
-        self.on_click = on_click
-        text_surface = font.render(text, True, text_color)
+        self.set_text(text)
+        self.enable = True
+
+    def set_text(self, text):
+        self.surface.fill(self.background)
+        text_surface = self.font.render(text, True, self.text_color)
         self.surface.blit(text_surface, ((self.rect.w - text_surface.get_width()) // 2,
                                          (self.rect.h - text_surface.get_height()) // 2))
 
-    def pgevent(self, event):
+    def pg_event(self, event):
+        pass
+
+    def draw(self, surface):
+        surface.blit(self.surface, (self.rect.x, self.rect.y))
+
+    def set_enable(self, enable):
+        self.enable = enable
+
+
+class Button(Label):
+    _font = pg.font.SysFont("Arial", 20, )
+
+    def __init__(self, rect, text, on_click=None, text_color="BLACK", background="WHITE", font=_font):
+        super(Button, self).__init__(rect, text, text_color, background, font)
+        self.on_click = on_click
+
+    def pg_event(self, event):
         if event.type == pg.MOUSEBUTTONDOWN:
             if event.button == pg.BUTTON_LEFT and self.rect.collidepoint(event.pos):
                 self.click()
@@ -109,7 +113,7 @@ class Button:
 
     def click(self):
         if self.on_click:
-            self.on_click()
+            self.on_click(self)
 
 
 class Capability:
@@ -118,12 +122,12 @@ class Capability:
             raise Exception("Capability init ERROR, variable 'options' is None")
         self.ancestor = options.get(S_ANCESTOR)
         if self.ancestor:
-            self.options = capabilities_options[self.ancestor].copy()
+            self.options = Capabilities[self.ancestor].copy()
             self.options.update(options)
         else:
             self.options = options
         self.name = self.options[S_TITLE]
-        self.ctype = self.options[S_CTYPE]
+        self.ctype = self.options[S_TYPE]
 
     def punch(self, other: Creature):
         damage = self.options.get(S_PUNCH_DAMAGE, -1)
@@ -139,22 +143,94 @@ class Capability:
             self.punch(other)
 
 
-class CapabilityPunch:
-    def __init__(self, damage):
-        super(CapabilityPunch, self).__init__({S_PUNCH_DAMAGE: damage, S_CTYPE: CAPABILITY_PUNCH,
-                                               S_TITLE: "Capability Punch"})
+# class CapabilityPunch:
+#     def __init__(self, damage):
+#         super(CapabilityPunch, self).__init__({S_PUNCH_DAMAGE: damage, S_TYPE: CAPABILITY_PUNCH,
+#                                                S_TITLE: "Capability Punch"})
+class Inventory:
+    def __init__(self, size=10):
+        self.size = size
+        self._lst = [None] * self.size
+
+    @property
+    def list(self):
+        return self._lst
+
+    def __getitem__(self, item):
+        return self._lst.__getitem__(item)
+
+    def add_item(self, item, cnt=1) -> int:
+        """Положить предметы. Возаращает сколько не вместилось"""
+        max_cnt = Items[item].get(S_MAX_CELL_CNT)
+        for i in range(self.size):
+            if self._lst[i] is None:
+                self._lst[i] = [item, 0]
+            if self._lst[i][0] == item:
+                cell_cnt = self._lst[i][1]
+                free = max_cnt - cell_cnt
+                if cnt > free:
+                    self._lst[i] = [item, max_cnt]
+                    cnt -= free
+                else:
+                    self._lst[i] = [item, cell_cnt + cnt]
+                    cnt = 0
+            if cnt == 0:
+                return 0
+        return cnt
+
+    def add_items(self, items):
+        """Положить список предметов"""
+        for item, cnt in items:
+            lost = self.add_item(item, cnt, )
+
+    def get_item(self, item, cnt=1) -> 0:
+        """Забрать преедметы. Возаращает сколько не хватило"""
+        for i in range(self.size):
+            if self._lst[i] is not None and self._lst[i][0] == item:
+                if self._lst[i][1] > cnt:
+                    self._lst[i][1] -= cnt
+                    cnt = 0
+                else:
+                    cnt -= self._lst[i][1]
+                    self._lst[i] = None
+                if cnt == 0:
+                    return 0
+        return cnt
+
+    def get_cnt_of_cell(self, cell_index, cnt=1):
+        """Взять кол-во из определенной ячейки. Возаращает сколько не хватило"""
+        if self._lst[cell_index] is not None:
+            self._lst[cell_index][1] -= cnt
+            if self._lst[cell_index][1] <= 0:
+                lost = -self._lst[cell_index][1]
+                self._lst[cell_index] = None
+                return lost
+        return
 
 
 class Player(Creature):
     def __init__(self):
-        options = {S_LIVES: 100, S_MANA: 0, S_IMGID: PLAYER_IMG_ID, S_TITLE: "Player"}
+        options = {S_HEALTH: 100, S_MANA: 0, S_IMGID: PLAYER_IMG_ID, S_TITLE: "Player"}
         capabilities = get_capabilities(["DefaultPunch", "LightPunch"])
         super(Player, self).__init__(options, capabilities, alive=True)
         self.battle = None
+        self.inventory = Inventory(size=10)
+        self.inventory.add_items([("HealthPoison", 3), ("RestoreHealthPoison", 1)])
+        self.coins = 0
+        self.effects = []
 
     def start_battle(self, opponents):
+        """Стартовать битву с оппонентами"""
         self.battle = Battle(self, opponents)
         return self.battle
+
+    def apply_item(self, cell_index):
+        item, cnt = self.inventory[cell_index]
+        prop = Items.get(item)
+        if prop[S_TYPE] == ITEM_POISON:
+            if prop.get(S_HEALTH):
+                self.add_health(prop.get(S_HEALTH))
+            self.inventory.get_cnt_of_cell(cell_index)
 
 
 PLAYER = 1
@@ -162,7 +238,7 @@ OPPONENT = 2
 
 
 class Battle:
-    def __init__(self, player, opponents):
+    def __init__(self, player: Player, opponents: List[Creature]):
         self.player = player
         self.opponents = opponents
         self.current_opponent = 0
@@ -170,12 +246,16 @@ class Battle:
         self.master = PLAYER
         self.win = 0
         self.running = True
+        self.in_room = True
 
     def player_punch(self, capability):
         if self.master == PLAYER:
             capability.start_action(self.opponents[self.current_opponent])
             self.next_opponent()
             return True
+
+    def player_apply_item(self, cell_index):
+        self.player.apply_item(cell_index)
 
     def opponent_move(self):
         for opp in self.opponents:
@@ -187,6 +267,7 @@ class Battle:
                     return
 
         self.master = PLAYER
+        self.next_opponent()
 
     def next_opponent(self):
         # след опонент когда ходит игрок
@@ -198,8 +279,11 @@ class Battle:
             self.running = False
             self.win = PLAYER
             return
-        self.current_opponent = 0
+        self.current_opponent = -1
         self.master = OPPONENT
+
+    def exit_at_room(self):
+        self.in_room = False
 
 
 running = True
@@ -209,14 +293,14 @@ def pg_update_iter(ui_objects=None):
     global running
     if ui_objects:
         for obj in ui_objects:
-            obj.draw(screen)
+            if obj.enable: obj.draw(screen)
     pg.display.flip()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
         if ui_objects:
             for obj in ui_objects:
-                obj.pgevent(event)
+                if obj.enable: obj.pg_event(event)
         yield event
 
 
@@ -228,27 +312,38 @@ font = pg.font.SysFont("Arial", 20)
 font_a1 = pg.font.SysFont("Arial", 40)
 
 
-def color_of_lives_percent(lives_percent):
+def color_of_health_percent(health_percent):
     color = "#64ee8b"
-    if player.lives_percent < 0.6:
+    if player.health_percent < 0.6:
         color = "#fef08a"
-    if player.lives_percent < 0.3:
+    if player.health_percent < 0.3:
         color = "#ef4444"
     return color
 
 
 def battle(player: Player, _opponents):
     battle = player.start_battle(_opponents)
-
+    end_ui = []
+    end_ui.append(Label(((WSIZE[0] - 200) // 2, (WSIZE[1] - 40) // 2, 200, 40),
+                        "YOU WIN" if battle.win == PLAYER else "YOU DIED",
+                        background="black", text_color="red", font=font_a1))
+    end_ui.append(Button(((WSIZE[0] - 200) // 2, (WSIZE[1] - 40) // 2+50, 200, 40), "Выйти из комнаты",
+                         on_click=lambda _b: battle.exit_at_room))
     ui_objects = []
-    y = 0
+    ui_objects.append(Label((10, 310, 220, 30), "Способности", text_color="white", background="black"))
+    ui_objects.append(Label((610, 310, 220, 30), "Инвентарь", text_color="white", background="black"))
+    y = 350
     for cap in player.capabilities:
-        ui_objects.append(Button((10, 300 + y, 120, 30), cap.name,
-                                 on_click=lambda _cap=cap: battle.player_punch(_cap)))
+        ui_objects.append(Button((10, y, 220, 30), cap.name,
+                                 on_click=lambda _b, _cap=cap: battle.player_punch(_cap)))
         y += 25 + 10
-    while running:
+
+    while running and battle.in_room:
+        print(running and battle.in_room, running , battle.in_room)
 
         screen.fill("black")
+
+        fast_ui = []
 
         # player
         x, y = 30, 50
@@ -258,11 +353,11 @@ def battle(player: Player, _opponents):
 
             text_surface = font.render(opp.name, True, "white", "black")
             screen.blit(text_surface, (x + (img_size[0] - text_surface.get_width()) // 2,
-                                             y + 3))
+                                       y + 3))
 
             pg.draw.rect(screen, "#64748b", ((x, y + 220 + 2), (220, 5)))
             if opp.alive:
-                pg.draw.rect(screen, "#64ee8b", ((x, y + 220 + 2), (220 * opp.lives_percent, 5)))
+                pg.draw.rect(screen, "#64ee8b", ((x, y + 220 + 2), (220 * opp.health_percent, 5)))
             else:
                 pg.draw.line(screen, "red", (x + 5, y + 5), (x + img_size[0] - 5, y + img_size[1] - 5), 5)
                 pg.draw.line(screen, "red", (x + img_size[0] - 5, y + 5), (x + 5, y + img_size[1] - 5), 5)
@@ -273,29 +368,33 @@ def battle(player: Player, _opponents):
             x += 250
             i += 1
 
-
-        screen.blit(font.render(f"Player: {player.lives}", True, "WHITE"), (0, 0))
+        screen.blit(font.render(f"Player: {player.health}HP", True, "WHITE"), (0, 0))
         pg.draw.rect(screen, "#64748b", ((5, 25), (300, 8)))
         if player.alive:
-            pg.draw.rect(screen, color_of_lives_percent(player.lives_percent),
-                         ((5, 25 + 1), (300 * player.lives_percent, 8 - 2)))
-
+            pg.draw.rect(screen, color_of_health_percent(player.health_percent),
+                         ((5, 25 + 1), (300 * player.health_percent, 8 - 2)))
 
         if battle.running:
+
+            y = 350
+            for i in range(player.inventory.size):
+                if player.inventory[i] is not None:
+                    item, cnt = player.inventory[i]
+                    fast_ui.append(Button((610, y, 220, 30), Items[item].get(S_TITLE) + f" ({cnt})",
+                                          on_click=lambda _b, _i=i: battle.player_apply_item(_i)))
+                y += 25 + 10
+
             if battle.master == OPPONENT:
                 battle.opponent_move()
-            pg_update(ui_objects)
+            pg_update(ui_objects + fast_ui)
         else:
-            text = font_a1.render("YOU WIN" if battle.win == PLAYER else "YOU DIED", True, "red")
-            screen.blit(text, ((WSIZE[0] - text.get_width()) // 2,
-                               (WSIZE[1] - text.get_height()) // 2))
 
-            pg_update()
+            pg_update(end_ui)
 
 
 player = Player()
+exit_battle = False
 opponents = [get_creature("BlackCat"), get_creature("BlackCat"), get_creature("CryCat")]
 battle(player, opponents)
-
 while running:
     pg_update([])
